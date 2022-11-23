488541be70491ecba8c9dd98daeaa20f640d7f74
AdB <abala@edu-soft.pro>
2022-11-23 22:53:35 +0100

    Added debug and pause tests

---
+++ cypress/e2e/d1/z4/debugger.cy.js
@@ -0,0 +1,49 @@
+/// <reference types="cypress" />
+
+// Task #4
+const todos = ["Learn Cypress", "Make a break", "Use more Cypress"];
+
+describe("Calling debugger", () => {
+  beforeEach(() => {
+    cy.visit("https://example.cypress.io/todo");
+  });
+
+  it(`Should run normally`, () => {
+    cy.get("[data-test=new-todo]")
+      .type(`Check debugger{enter}`)
+      .get(".todo-list li")
+      .should("have.length", 2 + 1); // ToDo has 2 items by default
+  });
+
+  it(`Should stop in debug mode randomly`, () => {
+    cy.get("[data-test=new-todo]");
+    debugger;
+  });
+
+  it(`Should stop in debug mode normally`, () => {
+    cy.get("[data-test=new-todo]").then(() => {
+      debugger;
+    });
+  });
+
+  it(`Should open debug mode normally`, () => {
+    cy.get("[data-test=new-todo]").debug();
+  });
+
+  it(`Should pause normally`, () => {
+    cy.get("[data-test=new-todo]")
+      .pause() // Should pause test run normally
+      .type(`Check debugger{enter}`)
+      .get(".todo-list li")
+      .should("have.length", 2 + 1); // ToDo has 2 items by default
+  });
+
+  it("Use debugger, debug() or pause() to fix it", () => {
+    cy.get("[data-test=new-todo]")
+      .type(`Check debugger{enter}`)
+      .get(".todo-list li")
+      .should("have.length", 2 + 1); // ToDo has 2 items by default
+    cy.contains("Active").click();
+    cy.get(".todo-list li").should("have.length", 1);
+  });
+});
