70401c0766a1550a81874ebb5e91439881e67dba
AdB <abala@edu-soft.pro>
2022-11-23 21:35:56 +0100

    Added hello cypress test

---
+++ cypress/e2e/d1/z1/hello.cy.js
@@ -0,0 +1,10 @@
+/// <reference types="Cypress" />
+
+// describe("Hello", () => {
+//   it("Cypress World!", () => {
+//     expect(true).to.equal(true);
+//   });
+// });
+context("", () => {
+  it("");
+});
