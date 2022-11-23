ea74f6e75469eab24738eab84a8d6423953ecad3
AdB <abala@edu-soft.pro>
2022-11-23 21:48:30 +0100

    Added chaining multiple assertions test

---
+++ cypress/e2e/d1/z2/assertions.cy.js
@@ -0,0 +1,21 @@
+/// <reference types="cypress" />
+
+// Task #2
+const todos = ['Learn Cypress', 'Make a break', 'Use more Cypress']
+
+describe('Chaining multiple assertions', () => {
+  beforeEach(() => {
+    cy.visit('https://example.cypress.io/todo')
+  })
+
+  it(`Should add all todos`, () => {
+    todos.forEach((todo, idx) => {
+      cy.get('[data-test=new-todo]')
+        .type(`${todo}{enter}`) 
+        .get('.todo-list li')
+        .should('have.length', 2 + idx + 1) // ToDo has 2 items by default
+        .last()
+        .should('have.text', todo)
+    })
+  })
+})
