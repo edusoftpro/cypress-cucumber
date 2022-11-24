b7ef8d6c23664793b9cef96ee54d98a07f02bdf8
AdB <abala@edu-soft.pro>
2022-11-24 23:15:22 +0100

    Added few experimental features such as WebKit (Safari) browser

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,13 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    experimentalRunAllSpecs: true,
+    experimentalStudio: true,
+    experimentalWebKitSupport: true,
+    retries: {
+      runMode: 0,
+      openMode: 1,
+    },
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
