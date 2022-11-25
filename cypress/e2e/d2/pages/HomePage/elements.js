1f15a2d1fb3fb29d9b22697c09c5aada0bca5d1c
AdB <abala@edu-soft.pro>
2022-11-25 02:51:36 +0100

    Added POM and custom text command

---
+++ cypress/e2e/d2/pages/HomePage/elements.js
@@ -0,0 +1,6 @@
+module.exports = {
+  HOMEPAGE: {
+    CONSENT_BUTTON: "div.QS5gu",
+    SEARCH_TXTBOX: "input[name='q']",
+  },
+};
--- cypress/e2e/d2/step_definitions/commonSteps.js
+++ cypress/e2e/d2/step_definitions/commonSteps.js
@@ -1,4 +1,9 @@
 import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
+import HomePage from "../pages/HomePage/HomePage";
+import ResultPage from "../pages/ResultPage/ResultPage";
+
+const homePage = new HomePage();
+const resultPage = new ResultPage();
 
 Given("I open Google homepage", () => {
   cy.visit("https://www.google.com");
@@ -6,4 +11,15 @@ Given("I open Google homepage", () => {
 
 Then("Title of web page is {string}", (title) => {
   cy.title().should("include", title);
+});
+
+When('I provide search query as {string}', (query) => {
+  homePage.clickConsentCookies();
+  homePage.clickSearchTxtBox();
+  homePage.typeInSearchTxtBox(query);
+  homePage.submitSearchQuery();
+});
+
+Then('Verify first search result to match {string} keyword', (search_keyword) => {
+  let result = resultPage.verifyFirstResult(search_keyword);
 });
